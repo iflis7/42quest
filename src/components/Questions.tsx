1fa7715dad89ce6e91c0de5dbdc6de975959a9be
bylkode <hafidsaadibk@gmail.com>
2022-12-23 15:38:41 -0500

    Question component

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-type QuestionsProps = {};
+type QuestionsProps = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number;
+};
 
 const Questions: React.FC<QuestionsProps> = (props: QuestionsProps) => {
-  return <div>questions</div>;
+  return (
+    <div>
+      <p className='number'>
+        Question: ({props.questionNr} / {props.totalQuestions})
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: props.question }} />
+      <div>
+        {props.answers.map((answer) => (
+          <div>
+            <button disabled={props.userAnswer} onClick={props.callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Questions;
