06c62d6011db73492f51791eef197689349de4c0
bylkode <hafidsaadibk@gmail.com>
2022-12-23 18:23:58 -0500

    Styling done. Error handling needed

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -12,14 +12,37 @@ type QuestionsProps = {
 
 const Questions: React.FC<QuestionsProps> = (props: QuestionsProps) => {
   return (
-    <div className='flex flex-col items-center'>
-      <p className='number '>
+    <div className='flex flex-col items-center mb-6'>
+      <p className='text-lg font-medium p-2 my-2 '>
         Question: {props.questionNr} / {props.totalQuestions}
+        {props.questionNr >= 2 ? (
+          <span
+            className='mx-2 bg-gray-100 text-emerald-600 text-sm font-semibold inline-flex 
+        items-center p-1.5 rounded-full mr-2 dark:bg-emerald-600 dark:text-gray-300'
+          >
+            <svg
+              aria-hidden='true'
+              className='w-3.5 h-3.5'
+              fill='currentColor'
+              viewBox='0 0 20 20'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                fill-rule='evenodd'
+                d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
+                clip-rule='evenodd'
+              ></path>
+            </svg>
+            <span className='sr-only'>Icon description</span>
+          </span>
+        ) : null}
       </p>
-      <p dangerouslySetInnerHTML={{ __html: props.question }} />
-      <div>
+      <p className="font-semibold p-4 text-lg" dangerouslySetInnerHTML={{ __html: props.question }} />
+      <div >
         {props.answers.map((answer) => (
-          <div key={answer}>
+          <div className="font-medium my-1.5 p-1 px-4 rounded-md
+          text-white bg-black hover:bg-yellow-300 hover:text-black
+          " key={answer}>
             <button
               disabled={props.userAnswer ? true : false}
               value={answer}
--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -3,7 +3,6 @@ import Questions from '../components/Questions';
 import { AnswerObject, fetchQuizQuestions } from '../API';
 import { QuestionState, Difficulty } from '../API';
 
-
 const TOTAL_QUESTIONS = 10;
 
 function Quiz() {
@@ -64,16 +63,25 @@ function Quiz() {
 
   return (
     <div
-      className="flex flex-col items-center mx-auto p-4 bg-white border border-gray-500 
+      className='flex flex-col items-center mx-auto p-4 bg-white border border-gray-500 
       rounded-md w-11/12 sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12 
-      shadow-lg h-2/3 sm:p-6 md:p-8"
+      shadow-md shadow-yellow-500 h-2/3 sm:p-6 md:p-8'
     >
-      <h1 className="m-2 text-3xl font-extrabold text-gray-900 dark:text-black 
-      md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r
-       to-emerald-600 from-sky-400">42 Quest</span> Quiz </h1>
+      <h1
+        className='m-2 text-3xl font-extrabold text-gray-900 dark:text-black 
+      md:text-5xl lg:text-6xl'
+      >
+        <span
+          className='text-transparent bg-clip-text bg-gradient-to-r
+       to-emerald-600 from-sky-400'
+        >
+          42 Quest
+        </span>{' '}
+        Quiz{' '}
+      </h1>
       {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
         <button
-          className='flex p-0.5 mb-2 mr-2 mt-4 px-6
+          className='flex self-center p-1  mb-2 mr-2 mt-40 px-6
       overflow-hidden text-sm font-medium text-gray-900 rounded-lg 
       group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400
        group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 
@@ -86,8 +94,38 @@ function Quiz() {
         </button>
       ) : null}
 
-      {!gameOver ? <h4 className="flex text-2xl py-2 font-bold text-black">Score: {score}</h4> : null}
-      {loading ? <p>Loading Quests...</p> : null}
+      {!gameOver ? (
+        <h4 className='flex text-2xl mt-4 py-2 font-bold text-black'>
+          Score:{' '}
+          <span
+            className='px-2 text-transparent bg-clip-text bg-gradient-to-r
+       to-emerald-600 from-sky-400'
+          >
+            {' '}
+            {score}{' '}
+          </span>
+        </h4>
+      ) : null}
+      {loading ? (
+        <div role='status'>
+          <svg
+            className='inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-yellow-400'
+            viewBox='0 0 100 101'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+              fill='currentColor'
+            />
+            <path
+              d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+              fill='currentFill'
+            />
+          </svg>
+          <span className='sr-only'>Loading...</span>
+        </div>
+      ) : null}
       {!loading && !gameOver ? (
         <Questions
           key={questions[number].question}
@@ -105,14 +143,14 @@ function Quiz() {
       userAnswers.length === number + 1 &&
       number !== TOTAL_QUESTIONS - 1 ? (
         <button
-          className='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 
+          className='relative inline-flex items-center justify-center p-1 mt-4 px-8
       overflow-hidden text-sm font-medium text-gray-900 rounded-lg 
       group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400
        group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none
         focus:ring-green-200 dark:focus:ring-green-800'
           onClick={nextQuestion}
         >
-          <span className='relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+          <span className='relative px-2 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
             Next Quest{' '}
           </span>
         </button>
